df95d64ef63c7003342e5418cf677caccad06d6c
Aadhavan Srinivasan <srini193@purdue.edu>
2024-04-11 11:34:51 -0400

    Updated script to work with IMU log files

--- coords_to_csv.sh
+++ coords_to_csv.sh
@@ -10,11 +10,46 @@
 # run './coords_to_csv.sh <logfile>', replacing <logfile> with the
 # name of the actual log file.
 
+# Exit the program if any command fails. Useful for debugging purposes.
+set -xeo pipefail
 
-file_to_read="$1"
+
+# This function checks if the program was run with the '--imu' flag.
+has_imu_param() {
+	imu_flag="--imu"
+	cmd_args="$1"
+
+	if grep -q -- "$imu_flag" <<< "$*"; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+if [ "$1" == "" ]; then
+	echo "ERROR: File name must be specified."
+	exit
+fi
+
+#Last argument to script
+file_to_read="${@: -1}"
+
 file_to_write="${file_to_read%.txt}.csv"
+temp_file="tmpfile.txt"
 
 echo "LATITUDE,LONGITUDE" > "$file_to_write"
+
+# If the program was run with the --imu flag, then we are looking at a file that contains IMU data in it.
+# In this case, here are the additional steps that must be performed before:
+# 1. Filter the contents of the file to only get the following things:
+#	a. GPS point before update
+#	b. Update
+#	c. GPS point after update
+# 2. Remove the headers for each subsection, and separate each section by a newline.
+
+if has_imu_param "$*"; then
+	awk '!/Refresh/ && (/IMU/ || /Lat/)' "$file_to_read" | sed -e '/BEFORE/c\\' -e '/IMU/d' | tail -n +2 | awk '{if ($1 == "")print ""; else print $2 "," $4}' | awk '!NF{$0="<NEW>"}1' | cat <(echo "<NEW>") - > "$temp_file"
+else
 
 # 1. Find the relevant lines of the file (Lat/Long and Refresh rate)
 # 2. Remove all newlines in the file (Makes it easier to replace text
@@ -22,6 +57,17 @@ echo "LATITUDE,LONGITUDE" > "$file_to_write"
 # 4. Append a newline to the end of the file
 # 5. Remove the first line of the file, which is just an empty line
 # 6. Convert the file to a CSV file by inserting commas between the relevant values
-# 6. Output to the correct file
+# 7. Output to the correct file
 
-grep -E 'Lat|GPS' "$file_to_read" | tr '\r\n' ' ' | sed 's/Lat/\nLat/g' | cat - <(echo "") | tail -n +2  | awk '{print $2 "," $4 "," $8}' - >> "$file_to_write"
+	grep -E 'Lat|GPS' "$file_to_read" | tr '\r\n' ' ' | sed 's/Lat/\nLat/g' | cat - <(echo "") | tail -n +2 | awk '{print $2 "," $4 "," $8}' - >> "$temp_file"
+fi
+
+# Remove duplicating entries from file, based on first two columns (only if --imu flag was NOT used)
+if ! has_imu_param "$*"; then
+	awk -F, '!a[$1 $2]++ {print ;}' "$temp_file"  > "$file_to_write"
+else
+	cp "$temp_file" "$file_to_write"
+fi
+
+# Remove temp file
+rm "$temp_file"
